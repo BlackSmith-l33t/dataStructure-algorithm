b0674f5c87e6fa872c1fcdebb111f67e9dbf3abe
tranquila <sdf383000@gmail.com>
2021-08-06 21:30:18 +0900

    no message

--- C++/Practice1/main.cpp
+++ C++/Practice1/main.cpp
@@ -118,121 +118,88 @@ public:
 		cir_list_it operator--(int)
 		{
 			cir_list_it it = *this;
-			--(*this); return it;
+			--(*this); 
+			return it;
 		}
 
-
-
-	};
-};
-
-
-
-class PlayList {
-public:
-	using track = PlayList_track;
-	using track_ptr = track*;
-private:
-	track_ptr head;
-	track_ptr tail;
-	track_ptr play = head;	
-
-public:
-	void push_front(string title)
-	{
-		auto new_track = new track{ title, NULL };
-		if (head != NULL)
-		{	
-			new_track->next = head;
-		}
-
-		if (tail == NULL)
+		friend bool operator==(const cir_list_it& it1, const cir_list_it& it2)
 		{
-			tail = head;
-		}
-	
-		head = new_track;				
-	}
-	
-	string pop_front()
-	{
-		auto first = head;
-		if (head)
-		{
-			head = head->next;
-			tail = head;
-			delete first;
-		}
-		return head->title;
-	}
-	
-	struct PlayList_track_iterator
-	{
-	private:
-		track_ptr ptr;
-	
-	public:
-		PlayList_track_iterator(track_ptr p) : ptr(p) {}
-
-		string& operator*() { return ptr->title; }
-
-		track_ptr get() { return ptr; }
-
-		PlayList_track_iterator& operator++()
-		{
-			ptr = ptr->next;
-			return *this;
+			return it1.ptr == it2.ptr;
 		}
 
-		PlayList_track_iterator operator++(int)
+		friend bool operator!=(const cir_list_it& it1, cir_listit& it2)
 		{
-			PlayList_track_iterator result = *this;
-			return *this;
-		}
-
-		friend bool operator==(const PlayList_track_iterator& left, const PlayList_track_iterator& right)
-		{
-			return left.ptr == right.ptr;
-		}
-
-		friend bool operator!=(const PlayList_track_iterator& left, const PlayList_track_iterator& right)
-		{
-			return left.ptr != right.ptr;
+			return it1.ptr != it2.ptr;
 		}
 	};
-	
-	PlayList_track_iterator begin() { return PlayList_track_iterator(head); }
-	PlayList_track_iterator end() { return PlayList_track_iterator(tail); }
-	PlayList_track_iterator begin() const { return PlayList_track_iterator(head); }
-	PlayList_track_iterator end() const { return PlayList_track_iterator(tail); }
 
-	void insert(track_ptr place, string title)
+	cir_list_it begin()
 	{
-		auto new_track = new track{ title, NULL };
-		if (place != NULL)
-		{
-			place->next = new_track;
-			new_track->prev = place;
-		}
+		return cir_list_it{ head };
 	}
 
-	string next()
-	{					
-		play = head->next;
-		
-		return play->title;
-	}
-
-	string previous()
+	cir_list_it begin() const
 	{
-		play = play->prev;
-		
-		if (play == head)
-		{
-			play = tail;
-		}
-		
-		return play->title;
+		return cir_list_it{ head };
 	}
 
+	cir_list_it end()
+	{
+		return cir_list_it{ head->prev };
+	}
+
+	cir_list_it end() const
+	{
+		return cir_list_it{ head->prev };
+	}
+
+	cir_list_it(const cir_list<T>& other) : cir_list()
+	{
+		for (const auto& i : other)
+			insert(i);
+	}
+
+	cir_list(const initializer_list<T>& il) : head(NULL), n(0)
+	{
+		for (const auto& i : il)
+			insert(i);
+	}
+
+	~cir_list()
+	{
+		while (size())
+		{
+			erase(*(head->data));
+		}
+
+		delete head;
+	}
 };
+
+struct playlist
+{
+	cir_list<int> list;
+
+	void insert(int song)
+	{
+		list.insert(song);
+	}
+
+	void erase(int song)
+	{
+		list.erase(song);
+	}
+
+	void loopOnce()
+	{
+		for (auto& song : list)
+			cout << song << " ";
+		cout << endl;
+	}
+};
+
+int main()
+{
+	playlist pl;
+	pl.insert(1);
+}
