7c93a41ec933f60d623b2407376dfd1351fda47c
tranquila <sdf383000@gmail.com>
2021-08-26 22:00:17 +0900

    no message

--- C++/Practice2/main.cpp
+++ C++/Practice2/main.cpp
@@ -5,69 +5,115 @@ using namespace std;
 
 #define CARD_NUMBER 13
 
-struct Player
+
+template <typename T>
+struct cardList_node
 {
+	T* card;
+	cardList_node* next;
+	
+	~cardList_node()
+	{
+		delete card;
+	}
+};
+
+template <typename T>
+struct cardList {
 public:
-	int cardList[CARD_NUMBER];
+	using node = cardList_node<T>;
+	using node_ptr = node*;
+
 private:
+	node_ptr firstCard;
 	size_t n;
 
 public:
-
-	Player() : n(0)
+	cardLIst() : n(0)
 	{
-		random_device rd;
-		mt19937 gen(rd());
-		uniform_int_distribution<> dist(1, 10);
-			
-		for (int i = 0; i < CARD_NUMBER; i++)
-		{
-			cardList[i] = dist(gen);
-		}
+		firstCard = new node{ NULL, NULL };
+
 	}
 
 	size_t size() const
 	{
 		return n;
-	}	
-
-	~Player()
-	{
-		delete cardList;
 	}
 
 
 
 };
 
-void GameTable()
-{
+class Player {
+public:
+	using node = cardList_node<T>;
+	using node_ptr = node*;
+	
+private:
 
-}
 
-void Shuffle()
-{
+public:
 
-}
+	Player()
+	{
+		random_device rd;
+		mt19937 gen(rd());
+		uniform_int_distribution<> dist(1, 10);
 
-void SelectRandomCard()
-{
+		auto new_card = new node{ dist(gen), NULL };
+		for (int i = 0; i < CARD_NUMBER; i++)
+		{
+			node_ptr = dist(gen);
+		}
+	}
 
-}
+	~Player()
+	{
+		delete cardList;
+	}
 
-void MatchTest()
-{
+	void GameTable()
+	{
 
-}
+	}
 
-bool IsWinner()
-{
-	return false;
-}
+	void Shuffle()
+	{
+
+	}
+
+	void SelectRandomCard()
+	{
+
+	}
+
+	void MatchTest()
+	{
+
+	}
+
+	bool IsWinner()
+	{
+		return false;
+	}
+
+	void printCard()
+	{
+		for (int i = 0; i < CARD_NUMBER; i++)
+		{
+			cout << cardList[i] << endl;
+		}
+	}
+
+};
+
 
 
 int main()
 {
 	Player player;
+	
+	player.printCard();
+
 
 }
