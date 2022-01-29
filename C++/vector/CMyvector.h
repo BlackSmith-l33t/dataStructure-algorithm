da8aa45d14adae448de2a32b9b42d18d0b77df66
Nobody <nobody133738@gmail.com>
2022-01-29 23:15:14 +0900

    iterator

--- C++/vector/CMyvector.h
+++ C++/vector/CMyvector.h
@@ -1,99 +1,155 @@
 #pragma once
 #include <assert.h>
-#include <math.h>
 
-template <typename T>
+#define DEFAULT_SIZE 10
+
+template <typename T>	
 class CMyVector
 {
 private:
-	T*			 pData;
-	unsigned int m_size; // 현재 사용량
-	unsigned int m_capacity; //허용량
+	T*	pDataArray;
+	int m_size;
+	int m_capacity;
 public:
 	CMyVector();
+	~CMyVector();
 
+private:
+	void re_allocate(unsigned int resize);  // vector의 저장소 크기를 재조정
+
+public:
 	void push_back(const T& data);
 	void pop_back();
+	
+	int size();
+	int capacity();
+
+	void clear();
+	bool empty();
+
 	T& operator[](int index);
-
-	unsigned int size();
-	unsigned int capacity();
-
 	T& at(int index);
+
+	class iterator
+	{
+		friend CMyVector;
+	private:
+		CMyVector* pVector;
+		T* pData;
+		int m_index;
+	public:
+		T& operator*(); //*iter;
+		iterator& operator++(); //++iter;
+		iterator operator++(int); //iter++;
+		iterator& operator--(); //--iter;
+		iterator operator--(int); //iter--;
+		bool operator==(const iterator& _other); //iter == otherIter;
+		bool operator!=(const iterator& _other); //iter != otherIter;
+	};
+
+	iterator begin();
+	iterator end();
+
+	void insert(const iterator& iter, const T& value);
+	void erase(const iterator& iter);
 };
 
 template<typename T>
 CMyVector<T>::CMyVector()
 {
-	pData = new T[10];
+	pDataArray = new T[DEFAULT_SIZE];
 	m_size = 0;
-	m_capacity = 10;
+	m_capacity = DEFAULT_SIZE;
 }
 
 template<typename T>
-void CMyVector<T>::push_back(const T & data)
+CMyVector<T>::~CMyVector()
 {
-	if (m_size >= m_capacity)
+	m_size = 0;
+	m_capacity = 0;
+}
+
+template<typename T>
+void CMyVector<T>::re_allocate(unsigned int resize)
+{
+	if (m_size > resize)
 	{
-		T* newData = new T[m_capacity * 2];
-		for (int i = 0; i < m_size; ++i)
-		{
-			newData[i] = pData[i];
-		}
-		delete[] pData;
-		pData = newData;
-		m_capacity *= 2;
+		resize = m_size;
 	}
-	pData[m_size] = data;
+
+	T* newDataArray = new T[resize];
+	for (int i = 0; i < m_size; ++i)
+	{
+		newDataArray[i] = pDataArray[i];
+	}
+
+	delete[] pDataArray;
+	pDataArray = newDataArray;
+	m_capacity = resize;
+}
+
+template<typename T>
+int CMyVector<T>::size()
+{
+	return m_size;
+}
+
+template<typename T>
+int CMyVector<T>::capacity()
+{
+	return m_capacity;
+}
+
+template<typename T>
+void CMyVector<T>::push_back(const T& data)
+{
+	if (m_size > m_capacity / 2)
+	{
+		re_allocate(m_capacity * 2);
+	}
+
+	pDataArray[m_size] = data;
 	m_size++;
 }
 
 template<typename T>
 void CMyVector<T>::pop_back()
 {
-	if (m_size == 0)
+	assert(m_size != 0);
+	
+	if (m_size < m_capacity / 4)
 	{
-		return;
+		re_allocate(m_capacity / 2);
 	}
-
 	m_size--;
-
-	int capacitySize = ceil(((float)m_capacity / 8)); //
-
-	if (m_size < capacitySize)
-	{
-		T* newData = new T[(int)(m_capacity / 2)];
-		for (int i = 0; i < m_capacity / 2; i++)
-		{
-			newData[i] = pData[i];
-		}
-		delete[] pData;
-		pData = newData;
-		m_capacity /= 2;
-	}
 }
 
 template<typename T>
-unsigned int CMyVector<T>::size()
-{
-	return m_size;
-}
-
-template<typename T>
-unsigned int CMyVector<T>::capacity()
-{
-	return m_capacity;
-}
-
-template<typename T>
-T & CMyVector<T>::operator[](int index)
+T& CMyVector<T>::operator[](int index)
 {
 	return at(index);
 }
 
 template<typename T>
-T & CMyVector<T>::at(int index)
+T& CMyVector<T>::at(int index)
 {
-	assert(!(index >= m_size));
-	return pData[index];
+	assert(!(m_size == 0));
+
+	return pDataArray[index];
 }
+
+template<typename T>
+void CMyVector<T>::clear()
+{
+	delete[] pDataArray;
+	m_size = 0;
+	m_capacity = 0;
+}
+
+template<typename T>
+bool CMyVector<T>::empty()
+{	
+	return m_size == 0 && pDataArray == nullptr;
+}
+
+
--- C++/vector/main.cpp
+++ C++/vector/main.cpp
@@ -1,78 +1,134 @@
 #include <iostream>
 #include <vector>
-#include <assert.h>
-#include "CMyvector.h"
+#include "CMyVector.h"
 
 using namespace std;
-
-template <typename T>
-void print_vectorData(vector<T> vec)
-{
-	cout << "vector data : ";
-	for (int i = 0; i < vec.size(); ++i)
-	{
-		cout << vec[i] << ", ";
-	}
-	cout << endl;
-}
-
-template <typename T>
-void print_vectorData(CMyVector<T> vec)
-{
-	cout << "vector data : ";
-	for (int i = 0; i < vec.size(); i++)
-	{
-		cout << vec[i] << ", ";
-	}
-	cout << endl;
-}
 
 int main()
 {
 	CMyVector<int> myVector;
-	cout << "push_back(10)" << endl;
+
 	myVector.push_back(10);
-	print_vectorData(myVector);
-	cout << "vector.size() = " << myVector.size() << endl;
-	cout << "vector.capacity() = " << myVector.capacity() << endl;
-
-	cout << "push_back(20)" << endl;
 	myVector.push_back(20);
-	print_vectorData(myVector);
-	cout << "vector.size() = " << myVector.size() << endl;
-	cout << "vector.capacity() = " << myVector.capacity() << endl;
-
-	cout << "push_back(30)" << endl;
 	myVector.push_back(30);
-	print_vectorData(myVector);
-	cout << "vector.size() = " << myVector.size() << endl;
-	cout << "vector.capacity() = " << myVector.capacity() << endl;
 
-	cout << "pop_back()" << endl;
+	cout << myVector.at(0) << endl;
+	cout << myVector[0] << endl;
+
 	myVector.pop_back();
-	print_vectorData(myVector);
-	cout << "vector.size() = " << myVector.size() << endl;
-	cout << "vector.capacity() = " << myVector.capacity() << endl;
+	myVector.pop_back();
+	myVector.pop_back();
+	//myVector.pop_back();
 
-	for (int i = 0; i < 10; i++)
-	{
-		myVector.push_back(i);
-		cout << "vector.size() = " << myVector.size() << endl;
-		cout << "vector.capacity() = " << myVector.capacity() << endl;
-	}
-
-	print_vectorData(myVector);
-
-	cout << "myVector.at(i) :" << myVector.at(2) << endl;
-	cout << "myVector[i] :" << myVector[2] << endl;
-
-	for (int i = 0; i < 10; i++)
-	{
-		myVector.pop_back();
-	}
 	
-	cout << "vector.size() = " << myVector.size() << endl;
-	cout << "vector.capacity() = " << myVector.capacity() << endl;
 
-	return 0;
+	//CMyVector<int>::iterator iter;
+
+	//// iterator 활용
+	//cout << "iter : ";
+	//for (iter = myVector.begin(); iter != myVector.end(); ++iter)
+	//{
+	//	cout << *iter << ",";
+	//}
+	//cout << endl << endl;
+
+	//// iter++ test
+	//iter = myVector.begin();
+	//cout << "iter++ : " << *(iter++) << endl;
+	//cout << "iter   : " << *iter << endl;
+	//cout << endl;
+
+	//// iter-- test
+	//cout << "iter-- : " << *(iter--) << endl;
+	//cout << "iter   : " << *iter << endl;
+	//cout << endl;
+
+	//// ++iter test
+	//++iter = myVector.begin();
+	//cout << "++iter : " << *(++iter) << endl;
+	//cout << "iter   : " << *iter << endl;
+	//cout << endl;
+
+	//// --iter test	
+	//cout << "--iter : " << *(--iter) << endl;
+	//cout << "iter   : " << *iter << endl;
+	//cout << endl;
+
+//#pragma region iter != otherIter test
+//
+//	iter = myVector.begin();
+//
+//	cout << "iter = myVector.begin()" << endl;
+//	cout << "iter != myVector.end()" << endl;
+//	if (iter != myVector.end())
+//	{
+//
+//		cout << "=> iterator의 위치가 서로 다릅니다." << endl;
+//	}
+//	else
+//	{
+//		cout << "=> iterator의 위치가 서로 같습니다." << endl;
+//	}
+//
+//	cout << "===================================" << endl << endl;
+//
+//	iter = myVector.end();
+//	cout << "iter = myVector.end()" << endl;
+//	cout << "iter != myVector.end()" << endl;
+//	if (iter != myVector.end())
+//	{
+//		cout << "=> iterator의 위치가 서로 다릅니다." << endl;
+//	}
+//	else
+//	{
+//		cout << "=> iterator의 위치가 서로 같습니다." << endl;
+//	}
+//	cout << "===================================" << endl << endl;
+//#pragma endregion
+//
+//#pragma region iter == otherIter test
+//
+//	iter = myVector.begin();
+//
+//	cout << "iter = myVector.begin()" << endl;
+//	cout << "iter == myVector.end()" << endl;
+//	if (iter == myVector.end())
+//	{
+//
+//		cout << "=> iterator의 위치가 서로 같습니다." << endl;
+//	}
+//	else
+//	{
+//		cout << "=> iterator의 위치가 서로 다릅니다." << endl;
+//	}
+//
+//	cout << "===================================" << endl << endl;
+//
+//	iter = myVector.end();
+//	cout << "iter = myVector.end()" << endl;
+//	cout << "iter == myVector.end()" << endl;
+//	if (iter == myVector.end())
+//	{
+//		cout << "=> iterator의 위치가 서로 같습니다." << endl;
+//	}
+//	else
+//	{
+//		cout << "=> iterator의 위치가 서로 다릅니다." << endl;
+//	}
+//#pragma endregion
+//
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
