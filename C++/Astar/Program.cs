7e608a9aeea76e20e151d3109ad554fac5e14758
DESKTOP-NK217EO\Melchizedek <nobody133738@gmail.com>
2022-04-02 22:31:39 +0900

    Astar 알고리듬 C++ -> C#으로 변환 중

---
+++ C++/Astar/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace console_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello New World!");
+        }
+    }
+}
