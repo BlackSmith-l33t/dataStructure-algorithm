bf7d43d52ce1a026d78c3a85de3fcefe4e03dd34
Frankenstein <blacksmith.l33t@gmail.com>
2022-07-05 08:24:35 +0900

    Fisher-Yates Shuffle

---
+++ C#/Shuffle/Shuffle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shuffle
+{
+    class Shuffle
+    {
+        public static Random rng = new Random();
+
+        static void Main(string[] args)
+        {
+
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+            Shuffle_List(list);
+
+            foreach (var item in list)
+            {
+                Console.Write(item);
+                Console.Write(" ");
+            }
+        }
+
+        public static void Shuffle_List<T>(List<T> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
+    
